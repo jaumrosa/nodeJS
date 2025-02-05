34e64b44959c703089a0f660b3b2acc095b980f8
jaumrosa <joao.rosa5@outlook.com>
2025-02-04 22:41:39 -0300

    Utilizando findOne para filtrar um usuário via ID no get

--- routes/users.js
+++ routes/users.js
@@ -40,4 +40,18 @@ module.exports = (app) =>{
         })
     });
 
+    const routeId = app.route('/users/:id');
+
+    routeId.get((req,res) =>{
+
+        db.findOne({_id:req.params.id}).exec((err,user)=>{
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
+            }
+        })
+    })
+
 };

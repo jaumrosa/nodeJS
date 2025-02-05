df7fcf329223a77c454ef86af79ac84e7e779a22
jaumrosa <joao.rosa5@outlook.com>
2025-02-04 22:29:59 -0300

    Adicionando e listando usuários com banco NeDB

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,47 @@
+const NeDB = require('nedb')
+const db = new NeDB({
+    filename:'users.db',
+    autoload: true
+});
+
 module.exports = (app) =>{
 
     app.get('/users', (req, res)=>{
 
-        res.statusCode= 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users:[{
-                name:'João',
-                email:'joaoteste@joao.com',
-                id:1
-            }]
-         });
+
+        db.find({}).sort({name:1}).exec((err, users)=>{
+
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else{
+
+                res.statusCode= 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+            }
+
+        })
     });
     
     app.post('/users', (req, res) =>{
-    
-        res.json(req.body);
+
+        db.insert(req.body, (err, user)=>{
+
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else{
+
+                res.status(200).json(user);
+            }
+        })
     });
 
 };

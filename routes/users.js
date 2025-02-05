1f5ce11ca736750ff2631d70a3cfadfcccef4571
jaumrosa <joao.rosa5@outlook.com>
2025-02-04 22:37:42 -0300

    Refatorando código para rotas e erros

--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,15 @@ const db = new NeDB({
 
 module.exports = (app) =>{
 
-    app.get('/users', (req, res)=>{
+    const route = app.route('/users');
+
+    route.get((req, res)=>{
 
 
         db.find({}).sort({name:1}).exec((err, users)=>{
 
             if(err){
-                console.log(`error: ${err}`);
-                res.status(400).json({
-                    error: err
-                });
+                app.utils.error.send(err, req, res);
             } else{
 
                 res.statusCode= 200;
@@ -28,15 +27,12 @@ module.exports = (app) =>{
         })
     });
     
-    app.post('/users', (req, res) =>{
+    route.post((req, res) =>{
 
         db.insert(req.body, (err, user)=>{
 
             if(err){
-                console.log(`error: ${err}`);
-                res.status(400).json({
-                    error: err
-                });
+                app.utils.error.send(err, req, res);
             } else{
 
                 res.status(200).json(user);
